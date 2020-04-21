e5bc4aec0a86eeedb8556be85dc24268659070f2
Sylvia Sesay <svsesay15@students.desu.edu>
2020-04-21 19:13:34 -0400

    testing

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var router = express.Router();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    res.render('index', { title: 'Index' });
+});
+
+module.exports = router;
